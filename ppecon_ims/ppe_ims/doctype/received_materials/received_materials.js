84425ce0c159d6e8fad532cd9d41cad42c6632c6
Altamash Raza <altamarhraza875@gmail.com>
2026-07-23 15:03:38 +0300

    Recived Material

--- ppecon_ims/ppe_ims/doctype/received_materials/received_materials.js
+++ ppecon_ims/ppe_ims/doctype/received_materials/received_materials.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, altamash@ppecon.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Incoming materials", {
+// frappe.ui.form.on("Received Materials", {
 // 	refresh(frm) {
 
 // 	},
--- ppecon_ims/ppe_ims/doctype/received_materials/test_received_materials.py
+++ ppecon_ims/ppe_ims/doctype/received_materials/test_received_materials.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestIncomingmaterials(FrappeTestCase):
+class TestReceivedMaterials(FrappeTestCase):
 	pass
