409efe404d06f1b2400a41baf7a6dc99e40509b1
Altamash Raza <altamarhraza875@gmail.com>
2026-08-03 17:22:49 +0300

    equipment changes

--- ppecon_ims/ppe_ims/doctype/equipment/equipment.py
+++ ppecon_ims/ppe_ims/doctype/equipment/equipment.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Equipement(Document):
+class Equipment(Document):
 	pass
--- ppecon_ims/ppe_ims/doctype/equipment/test_equipment.py
+++ ppecon_ims/ppe_ims/doctype/equipment/test_equipment.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestEquipement(FrappeTestCase):
+class TestEquipment(FrappeTestCase):
 	pass
